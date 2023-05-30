88c842c8bdfbe5972f7ceec0779ed7abe9ca2452
煦峖 <1223565009@qq.com>
2023-05-31 01:17:35 +0800

    gateway配置

---
+++ Service_Provider_11000/src/main/java/com/provider/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.provider.controller;
+
+import com.provider.entity.CommentResult;
+import com.provider.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello";
+    }
+
+    @GetMapping("/getUserById/{userId}")
+    public CommentResult<User> getUserById(@PathVariable Integer userId){
+        User u = new User(userId,"小明","123456");
+        return new CommentResult<>(200,"success(11000)",u);
+    }
+}
